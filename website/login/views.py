294defd29935f8464b2d592436f9107900be2f85
frankwsj <sijun.wang@gmail.com>
2015-12-27 20:12:58 +0800

    login v2

--- website/login/views.py
+++ website/login/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render,render_to_response
 from django.http import HttpResponseRedirect
 from login.models import User
 from django import forms
+from django.contrib.auth import authenticate,login,logout
 
 
 # 定义表单模型
@@ -24,11 +25,18 @@ def login(request):
             username = uf.cleaned_data['username']
             password = uf.cleaned_data['password']
             # 获取的表单数据与数据库进行比较
-            user = User.objects.filter(username__exact = username,password__exact = password)
-            if user:
-                return render_to_response('success.html',{'username':username})
+            #user = User.objects.filter(username__exact = username,password__exact = password)
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return render_to_response('success.html', {'username': username})
             else:
-                return HttpResponseRedirect('/login/')
+                    return render_to_response('success.html', {'username': username})
+                    #return HttpResponseRedirect('/login/')
+
+        else:
+            pass
     else:
         uf = UserForm()
     return render_to_response('login.html',{'uf':uf})
